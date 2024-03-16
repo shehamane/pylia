66d890c9cfc0e4fcfcbd137fda2c9012a71663c0
shehamane <shehamane@mail.ru>
2024-03-16 09:48:37 +0300

    out dir added to .gitignore

--- out/2.jl
+++
@@ -1,3 +0,0 @@
-x::Int64 = 1
-y = 2
-println(x .+ y)
--- out/3.jl
+++
@@ -1,9 +0,0 @@
-function main()
-    x = [1, 2, (2 .+ 2 .* 2)]
-    y::Float64 = 1e-16
-    println(x .+ y)
-end
-
-for i in 1:10 
-    main()
-end
